b8cc8a01952bd70ad48c112f320923658abac9a0
grGrimales <grojasgrimales@gmail.com>
2025-07-16 18:28:42 -0300

    preparar produccion

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,26 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // cros
+ // --- CONFIGURACIÓN DE CORS MEJORADA ---
+  const whitelist = [
+    'http://localhost:4200', // Para desarrollo local
+    'https://idiomas-app-fe.vercel.app', // Tu dominio de producción
+    'https://idiomas-app.vercel.app'
+  ];
+
   app.enableCors({
-    origin: '*', // Permite todas las solicitudes CORS
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        // Si el origen está en la lista blanca (o no hay origen, como en Postman), permite la petición
+        callback(null, true);
+      } else {
+        // Si no, rechaza la petición
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true, // Permite que se envíen cookies y headers de autorización
   });
+  // ------------------------------------
 
   await app.listen(process.env.PORT ?? 3000);
 }
